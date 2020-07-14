5415070fbdfa149827486c4b1e34ab2a28b72b2e
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-07-14 17:52:12 +0530

    URL Shortner part 1

---
+++ urlshort/handle.go
@@ -0,0 +1,16 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+func MapHandler(pathstoUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathstoUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
---
+++ urlshort/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"urlshort"
+)
+
+func main() {
+
+	mux := defaultMux()
+
+	pathstoUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	mapHandler := urlshort.MapHandler(pathstoUrls, mux)
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
